b78742e4c6d378320a4e20eaac25c511b66435b1
taejin <taejin5314@gmail.com>
2021-11-13 19:02:20 -0500

    generate and export the types

--- project/src/app.ts
+++ project/src/app.ts
@@ -1,5 +1,6 @@
 import axios, { AxiosResponse } from 'axios';
 import * as Chart from 'chart.js';
+import { CovidSummaryResponse } from './covid';
 
 // utils
 function $(selector: string) {
@@ -38,13 +39,6 @@ function createSpinnerElement(id: string) {
 // state
 let isDeathLoading = false;
 const isRecoveredLoading = false;
-
-interface CovidSummaryResponse {
-  Countries: any[];
-  Date: string;
-  Global: object;
-  Message: string;
-}
 
 // api
 function fetchCovidSummary(): Promise<AxiosResponse<CovidSummaryResponse>> {
